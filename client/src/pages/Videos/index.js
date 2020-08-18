e83630dfa08e895268451ba9a124c3d095a23fe3
eradicador <janisepinto@yahoo.com>
2020-08-18 11:13:08 -0400

    added model, seed for video and questions

--- client/src/pages/Videos/index.js
+++ client/src/pages/Videos/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Header, Message } from "semantic-ui-react";
 import ReactPlayer from "react-player"
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+
 
 
 export const Videos = () => {
@@ -22,6 +25,49 @@ export const Videos = () => {
             </div>
         </div>
     )
+        < CardGroup >
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This card has supporting text below as a natural lead-in to additional
+              content.{' '}
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This card has even longer content than the first to
+                    show that equal height action.
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
+        </CardGroup >
+    
 }
 
 export default Videos;
