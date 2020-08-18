19c30c96df8e3293b0e14fed0d45ced4c36a43d5
eradicador <janisepinto@yahoo.com>
2020-08-18 12:13:25 -0400

    fixed merch page

--- client/src/pages/Videos/index.js
+++ client/src/pages/Videos/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Header, Message } from "semantic-ui-react";
-import ReactPlayer from "react-player"
-import Card from 'react-bootstrap/Card'
-import CardGroup from 'react-bootstrap/CardGroup'
+import ReactPlayer from "react-player";
+import Card from "react-bootstrap/Card";
+import { CardDeck } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
 
 
 
@@ -13,61 +14,68 @@ export const Videos = () => {
                 <Header size="huge"> Tutorial Videos </Header>
                 <p>This is a Protected Route</p>
             </Message>
-            <div>
-                <ReactPlayer
-                    url="https://youtu.be/32oHIYU4pk8"
-                />
-                <div>
+
+            <CardDeck>
+                <Card>
+                    <ReactPlayer
+                        url="https://youtu.be/32oHIYU4pk8/"
+                        fluid={false}
+                        width={490}
+                        height={360}
+                    />
+                    <Card.Body>
+                        <Card.Title>1st Tutorial</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+            </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
                     <ReactPlayer
                         url="https://youtu.be/JmwIUBGBb9I"
+                        fluid={false}
+                        width={490}
+                        height={360}
                     />
-                </div>
-            </div>
-        </div>
-    )
-        < CardGroup >
-        <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-            </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-        </Card>
-        <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This card has supporting text below as a natural lead-in to additional
+                    <Card.Body>
+                        <Card.Title>2nd Tutorial</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional
               content.{' '}
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-        </Card>
-        <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This card has even longer content than the first to
-                    show that equal height action.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card> 
+                <Card>
+                    <ReactPlayer
+                        url="https://youtu.be/CMQp0bwjokw"
+                        fluid={false}
+                        width={490}
+                        height={360}
+                    />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This card has even longer content than the first to
+                            show that equal height action.
             </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-        </Card>
-        </CardGroup >
-    
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardDeck >
+        </div>
+
+    )
+
 }
 
 export default Videos;
--- controllers/merchController.js
+++ controllers/merchController.js
@@ -1,16 +1,16 @@
-const Merch = require("../models/merchModel");
+const db = require("../models/merchModel");
 
 // Defining methods for the merchController
 module.exports = {
   findAll: function(req, res) {
-    Merch
+    db.Merch
       .find(req.query)
       .sort({ name: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    Merch
+    db.Merch
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
